a7ffd8ff9b775acd9ed88e7074189ec423c7bce4
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-01-29 00:10:09 +0100

    all iterations solved

--- app.js
+++ app.js
@@ -2,13 +2,18 @@
 // Here you should import the required packages for your Express app: `express` and `morgan`
 const express = require("express");
 const logger = require("morgan");
-const projects = require("./data/projects.json")
+const projects = require("./data/projects.json");
+const articles = require("./data/articles.json");
+require("dotenv").config();
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
-const app = express();
+const app = express(); 
 
 const PORT = 5005;
+// const PORT = process.env.PORT;
+
+console.log("THIS IS PORT: ", PORT);
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
@@ -19,22 +24,28 @@ app.use(express.json());
 // - `morgan` logger to log all incoming requests
 app.use(logger("dev"));
 
-
 // ROUTES
 // Start defining your routes here:
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/home.html")
-})
+  res.sendFile(__dirname + "/views/home.html");
+});
 
 app.get("/blog", (req, res) => {
-    res.sendFile(__dirname + "/views/blog.html")
-})
+  res.sendFile(__dirname + "/views/blog.html");
+});
 
 app.get("/api/projects", (req, res) => {
-    res.json(projects);
-    // res.sendFile(__dirname + "/data/projects.json")
-})
+  res.json(projects);
+});
+
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/views/not-found.html");
+});
+
 // START THE SERVER
 // Make your Express server listen on port 5005:
-app.listen(PORT, () => console.log(`server running on port ${PORT}`)
-)
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
